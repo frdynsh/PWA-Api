34440a95a86a7a29f7b8c81ce9eec56862acbd87
ferdiyansah <frdynsh11@gmail.com>
2025-02-26 23:40:33 +0700

    update db.json

--- js/script.js
+++ js/script.js
@@ -1,29 +1,54 @@
 $(document).ready(function(){
-    var_url = "https://my-json-server.typicode.com/frdynsh/PWA_api/products"
+    var var_url = "https://my-json-server.typicode.com/frdynsh/PWA_api/products";
 
-    var dataResults = ''
-    var catResults = ''
-    var categories = []
+    var dataResults = '';
+    var catResults = '';
+    var categories = [];
 
-    $.get(_url, function(data){
+    $.get(var_url, function(data){
 
         $.each(data, function(key, items){
-
-            _cat = items.category
             
+            _cat = items.category;
+
             dataResults += "<div>"
                             + "<h3>" + items.name + "</h3>"
-                            + "<p>" + _cat + "< /p>" 
-                        "</div>";
+                            + "<p>" + _cat + "</p>" 
+                        + "</div>";
 
-            if($.inArray(_cat, categories) === -1){
-                categories.push(_cat)
-                optionResults += "<option value'"+ _cat +"'>" + _cat + "</option>"
+            if($.inArray(_cat, categories) == -1){
+                categories.push(_cat);
+                catResults += "<option value'"+ _cat +"'>" + _cat + "</option>";
             }
-        })
 
-        $('#products').html(dataResults)
-    $('#cat_select').html("option value='all'>semua</option>" + catResults)
+        });
 
-    })
-})
+        $('#products').html(dataResults);
+        $('#cat_select').html("<option value='all'>Semua</option>" + catResults);
+    });
+
+    $('#cat_select').on('change', function(){
+        var catSelect = $(this).val();
+        var dataFilter = '';
+
+        if(catSelect == 'all'){
+            $('#products').html(dataResults);
+            return;
+        }
+
+        $.get(var_url, function(data){
+            $.each(data, function(key, items){
+                _cat = items.category;
+
+                if(_cat == catSelect){
+                    dataFilter += "<div>"
+                                    + "<h3>" + items.name + "</h3>"
+                                    + "<p>" + _cat + "</p>" 
+                                + "</div>";
+                }
+            });
+
+            $('#products').html(dataFilter);
+        });
+    });
+});
